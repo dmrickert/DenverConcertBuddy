b62e7e334f49825114bae952b820c6e8d91089e3
dabbler <dmrickert@gmail.com>
2016-04-25 21:49:44 -0600

    Shell directory structure for app

---
+++ src/denver-concert-buddy.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+import googley_calendar
+import westwood_scrapey
+
+def main( *args ):
+    print('This')
+
+if __name__ == '__main__':
+    main()
---
+++ src/westwood_scrapey.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+class Scrapey:
+    def __init__(self, url):
+        self.url(url)
+        #self.
+        self.webpageData('')
+    def get_webpage(self):
+        # Logic to get webpage data
+        self.webpageData( 'test' )
